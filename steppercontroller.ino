feba9368d8d45dae3730fcca3b26acb5da8efa0f
RIU <info@rc-board.de>
2016-03-26 01:59:00 +0100

    Interrupts eingebaut und Menü vorbereitet

--- steppercontroller.ino
+++ steppercontroller.ino
@@ -37,16 +37,15 @@
 #define D7_pin 7                              //... LCD Pins
 
 // Eingangs-Pins
-#define POTI     A1                           // Analogeingang fuer Poti
-#define SWITCH    2                           // Geschwindigkeits-Wahlschalter
-#define ENC_CLOCK 5                           // Encoder Takt
-#define ENC_DATA  6                           // Encoder Signal
-#define ENC_SW    4                           // Encoder Druck-Taster
+#define ENC_CLOCK 49                          // Encoder Takt
+#define ENC_DATA  47                          // Encoder Signal
+#define ENC_SW    51                          // Encoder Druck-Taster
+#define MENU      53                          // Menu-Taster
 
 // Poti-Paramter
 #define MITTE     0                           // Encoder Mittenstellung
 #define ESTMIN    0                           // Minwert vom Encoder
-#define ESTMAX  400                           // Maxwert vom Encoder je höher desto feinfühliger ist die Steuerung
+#define ESTMAX   40                           // Maxwert vom Encoder je höher desto feinfühliger ist die Steuerung
 
 // Allegemeine Parameter
 #define Fmin      4                           // Minimale Schrittfrequenz - nicht kleiner als 1
@@ -71,12 +70,15 @@ int mmMaxTravelZ  = 460;                      // Maximaler Verfahrweg der Z-Achs
 
 char cBuffer[10], cLogValue[100], cDisplayValue[20]; // Variablen für  Ausgabe
 
+long rFreq = 0;                               // Ausgelesene Timer Frequenz
 long t = ESTMIN;                              // Anfänglicher Takt
 bool rightRotate;                             // Rechts rum
+bool bTimerRunning  = LOW ;
 
 volatile int iEncVal = 0;                     // Encoderwert
+volatile int iAbsEncVal = 0;
 volatile int iOldEncVal = 0;                  // Encoderwert
-
+volatile int change = 0;
 volatile long lCounter = 0;                   // Counter für das Timer-Callback (DEBUGGING)
 volatile bool hase;                           // HIGH = Hasengang, LOW = Schneckengang
 volatile int  z_pos_count;                    // Counter der Steps
@@ -116,27 +118,25 @@ void setup( void ) {
   pinMode( READY, OUTPUT );                     // Bereitschaftsanzeige
   pinMode( ZSTEP, OUTPUT );                     // Z Achsen Step
   pinMode( ZDIRP, OUTPUT );                     // Z Achsen Richtungs-Pin
-  pinMode( POTI, INPUT );                       // Poti Pin als INPUT
-  pinMode( SWITCH, INPUT );                     // Geschwindigkeits-Wahlschalter
   pinMode( ENC_DATA, INPUT );            // Encoder Daten
   pinMode( ENC_CLOCK, INPUT );           // Encoder Clock
   pinMode( ENC_SW, INPUT );                     // Encoder Taster
   pinMode( SLED, OUTPUT );                      // Eilgang LED
+  pinMode( MENU, INPUT );                       // Menu-Taster
   
   //Vorbelegungen
-  digitalWrite( SWITCH, HIGH );                 // Pullup
   digitalWrite( ENC_SW, HIGH );                 // Pullup
+  //digitalWrite( MENU, HIGH );
   digitalWrite( READY, LOW );                   // Erstmal nicht READY
   
   Timer0.attachInterrupt( zTimer );             // Timer0 Interrupt für die Z-Achse
-  
-  Timer1.attachInterrupt( wTimer );             // Timer1 für Displayaktualisierung alle 100 ms
-  Timer1.setFrequency( 4 );                     // Timer1 4Hz
-  Timer1.start();                               // Refresh starten
-  
+    
   attachInterrupt(digitalPinToInterrupt(ENC_CLOCK),isr_enc,RISING);
   attachInterrupt(digitalPinToInterrupt(ENC_DATA),isr_enc,FALLING);
-  
+  attachInterrupt(digitalPinToInterrupt(ENC_SW),isr_sw,FALLING);
+  debugLog ( "Starte 1\n" );
+  attachInterrupt(digitalPinToInterrupt(MENU), isr_menu, FALLING);  
+  debugLog ( "Starte 2\n" );
   
   digitalWrite( READY, HIGH );                  // Bereit
 
@@ -149,7 +149,35 @@ void setup( void ) {
 
 /* Encoder Interrupt-Routine */
 void isr_enc( void ){
-  //Nichts zu tun
+  
+  change = getEncoderTurn();                // Encoderwert holen
+  iEncVal = iEncVal + change;                   // Verstellwert mit Istwert abgleichen
+  iAbsEncVal = abs( iEncVal );              // Wert für die Timerberechnung ohne Vorzeichen (das machen wir ja mit ZDIRP
+
+}
+
+void isr_sw(void){
+  
+    if( iAbsEncVal != 0 ){                      // un der aktuelle Wert ist ungleich 0
+        iEncVal = 0;                            // wird der Speed genullt und der Stepper sollte sofort stoppen
+        iAbsEncVal = 0;
+        debugLog( "Stop ausgef.\n" );
+    }else{
+      debugLog( "Hase ausgef.\n" );
+      // ToDo Eilgang toggeln
+      hase = !hase;
+      // 1 Sekunde halen Menü aufrufen
+    }
+}
+
+void isr_menu(void){
+  int i = 0;
+  lcd.clear();                                  // Erstmal alles löschen
+  lcdWrite( 0, 0, "Menu:" );
+  while(i<10){
+      lcdWrite(1,0,itoa(i,cBuffer,10));
+      i++;
+  }
 }
 
 /* write Timer um die Ausgabe am Display zu verlangsamen (Ghosting) */
@@ -158,6 +186,9 @@ void wTimer( void ){
 }
 /* Z-Achsen Timer Routine */
 void zTimer( void ){
+  
+  Timer0.setFrequency(t);
+  Timer0.start();
   int step=!digitalRead( ZSTEP );
   digitalWrite( ZSTEP,step );
   
@@ -170,9 +201,9 @@ void zTimer( void ){
 
 void lcdSetup( void ){
   
-  lcd.setBacklightPin( 3, POSITIVE );           //LCD init und ausgabe starten
+  lcd.setBacklightPin( 3, POSITIVE );           //LCD init und Ausgabe starten
   lcd.setBacklight( 1 );
-  lcd.begin( 20, 4 );                           // oder lcd.begin(16,2); für meine 16/2 Displays
+  lcd.begin( 20, 4 );                           // 20 Zeichen mit 4 Zeilen initialisieren
   lcd.setCursor( 0, 0 );
   lcd.clear();
   lcd.print( "Initialisiere..." );
@@ -181,9 +212,9 @@ void lcdSetup( void ){
 void lcdInit( void ) {
 
   lcd.clear();                                  // Erstmal alles löschen
-  lcdWrite( 0, 0, "Geschw.:" );
-  lcdWrite( 2, 0, "Z Pos  :" );
-  lcdWrite( 2, 12, "        " );
+  lcdWrite( 0, 0, "mm/s :" );
+  lcdWrite( 1, 0, "Z Pos:" );
+  lcdWrite( 1, 12, "        " );
 
 }
 
@@ -194,22 +225,35 @@ void lcdWrite ( int iRow, int iCol, String sValue ) {
 
 /*Daten schreiben*/
 void writeData( void ) {
+  float fWegSek;                
+  lcdWrite( 0, 0, "mm/s :" );                     // Zeile 1 des Displays schreiben
+  if ( bTimerRunning ){
+    if ( hase ){
+      fWegSek = rFreq*mmPerStepZ/0.8;             // Verfahrgeschwindigkeit / REFAKOR
+    }else{
+      fWegSek = rFreq*mmPerStepZ;
+    }
+    lcdWrite( 0, 12, dtostrf(fWegSek, 4, 3, cBuffer) );    
+  }else{
+     lcdWrite( 0, 12, "0.000" );       
+  }
   
-  lcdWrite( 0, 0, "Geschw.:" );                   // Zeile 1 des Displays schreiben
   if ( hase ) {
-    lcdWrite(0, 8, "Schnell     ");
+    lcdWrite(0, 19, "H");
   } else {
-    lcdWrite(0, 8, "Langsam");
+    lcdWrite(0, 19, "L");
   }
   if ( rightRotate ){
-      lcdWrite( 0, 19, "^" );                     // Richtung für Z-Achse
+      lcdWrite( 1, 19, ">" );                     // Richtung für Z-Achse rauf
   }else{
-      lcdWrite( 0, 19, "v" );                     // Richtung für Z-Achse
+      lcdWrite( 1, 19, "<" );                     // Richtung für Z-Achse runter
   }
-      
+  if ( !bTimerRunning ){
+     lcdWrite( 1, 19, "X" );                      // Richtung für Z-Achse Stop
+  }
   
-  lcdWrite(2, 0, "Z Pos  :");                   // Zeile 3 des Displays schreiben
-  lcdWrite(2, 10, dtostrf(pos, 7, 3, cBuffer));
+  lcdWrite(1, 0, "Z Pos:");                       // Zeile 3 des Displays schreiben
+  lcdWrite(1, 10, dtostrf(pos, 7, 3, cBuffer));
 
 //  debugLog( "Z-Pos: " );
 //  debugLog( dtostrf( pos, 8, 3, cBuffer ));
@@ -225,12 +269,8 @@ void debugLog ( String sValue ){
 /******************************** Loop ************************************/
 void loop( void ) {
   
-                                 
-  int change = getEncoderTurn();                // Encoderwert holen
-  iEncVal = iEncVal + change;                   // Verstellwert mit Istwert abgleichen
-  int iAbsEncVal = abs( iEncVal );              // Wert für die Timerberechnung ohne Vorzeichen (das machen wir ja mit ZDIRP
-  
-  if(digitalRead( ENC_SW ) == LOW ){            // Wenn der Taster gedrückt wird
+                                  
+/*  if(digitalRead( ENC_SW ) == LOW ){          // Wenn der Taster gedrückt wird
     if( iAbsEncVal != 0 ){                      // un der aktuelle Wert ist ungleich 0
         iEncVal = 0;                            // wird der Speed genullt und der Stepper sollte sofort stoppen
         iAbsEncVal = 0;
@@ -238,7 +278,7 @@ void loop( void ) {
       // ToDo Eilgang toggeln
       // 1 Sekunde halen Menü aufrufen
     }
-  }
+  }*/
 
 #if RICHTUNG > 0
   rightRotate = (iEncVal < 0) ? HIGH : LOW ;    // Richtungs-Pin fuer Stepper
@@ -258,18 +298,28 @@ void loop( void ) {
       } else {
         t = map( iAbsEncVal, ESTMIN, ESTMAX, Fmin, Fmax / RFAKTOR )*2;
       }
+      
+      if (!bTimerRunning){
+        bTimerRunning = HIGH;
+        Timer0.setFrequency(t);
+        Timer0.start();
+      }
       debugLog( "real:" );
       debugLog( dtostrf( iEncVal, 4,0, cBuffer ) );
       debugLog( " gemappt:" );
       debugLog( ltoa( t,cBuffer, 10 ) );
+      debugLog(  " Realer Takt" );
+      rFreq = Timer0.getFrequency();
+      debugLog( ltoa(rFreq,cBuffer, 10 ) );
+
       debugLog( "\n" );
-      Timer0.setFrequency(t);
-      Timer0.start();
+
     }
   }else{
     if ( iAbsEncVal>0 ){                         // Wenn das Poti auf Anschlagswert ist nichts machen
      iEncVal = iOldEncVal;
     }else{                                       // Poti ist auf 0
+      bTimerRunning = LOW;
       Timer0.stop();                             // Timer0 stoppen
       digitalWrite( ZSTEP,LOW );                 // Zur Sicherheit noch Step auf LOW setzen um sicher zu sein das gepullt wird
     }
@@ -278,6 +328,7 @@ void loop( void ) {
   // Position berechnen
   pos = mmPerStepZ * z_pos_count;
 
+  writeData();
 }
 /*************************** Loop Ende ************************************/
 
